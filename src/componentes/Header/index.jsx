f2047b6d4fac9d7839e37fb6f62e59bde92c0192
Edgar Fernando <edgarrod71@gmail.com>
2024-07-04 20:39:55 -0500

    pequeña actualización

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -9,7 +9,7 @@ class Header extends Component {
         return (
             <>
             <section className="header">
-               <img src="src/img/header.png" alt="Org"/>
+               <img src="./img/header.png" alt="Org"/>
             </section>
             </>
         );
