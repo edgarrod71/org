549b75aacb42f0959ddd76f6e37426fcecf47231
Edgar Fernando <edgarrod71@gmail.com>
2024-07-04 23:39:08 -0500

    deploy config

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,16 +1,17 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage: "url(../../src/img/footer.png)" }}>
+    return <footer className='footer' style={{ backgroundImage: "url('./src/img/footer.png')" }}>
         <div className='redes'>
+           <h1>Cielo</h1>
             <a href='https://www.aluracursos.com/'>
-                <img src="src/img/facebook.png" alt='Facebook' />
+                <img src="../src/img/facebook.png" alt='Facebook' />
             </a>
             <a href='https://www.aluracursos.com/'>
-                <img src="src/img/twitter.png" alt='twitter' />
+                <img src="../org/src/img/twitter.png" alt='twitter' />
             </a>
             <a href='https://www.aluracursos.com/'>
-                <img src="src/img/instagram.png" alt='instagram' />
+                <img src="./src/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='src/img/Logo.png' alt='org' />
